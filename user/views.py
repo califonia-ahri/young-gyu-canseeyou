7af2855d2de0dfafff084555674081c0e77b5613
SGNAN <giananews@gmail.com>
2022-08-19 16:45:41 +0900

    added permissions and revise admin page

--- user/views.py
+++ user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.object.all()
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        token = serializer.validated_data
+        return Response({"token":token.key}, status=status.HTTP_200_OK)
+
+class ProfileView(generics.RetrieveAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
