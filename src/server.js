4e809395ff1b7b754203dc1b5376bfc0b22e2e0f
Woo-bi <39858246+Choo-bi@users.noreply.github.com>
2022-08-20 21:32:55 +0900

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -23,66 +23,66 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    },
 });
 
 instrument(wsServer, {
-  auth: false,
+    auth: false,
 });
 
 function publicRooms() {
-  const sids = wsServer.sockets.adapter.sids;
-  const rooms = wsServer.sockets.adapter.rooms;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
+    const sids = wsServer.sockets.adapter.sids;
+    const rooms = wsServer.sockets.adapter.rooms;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
 }
 
 function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+    return wsServer.sockets.adapter.rooms.get(roomName).size;
 }
 
 wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
+    socket["nickname"] = "Anon";
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    });
 
-  //방에 들어왔을 때
-  socket.on("room", (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
+    //방에 들어왔을 때
+    socket.on("room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
 
-  //방에 연결이 끊길 때
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-  });
+    //방에 연결이 끊길 때
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) =>
+            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+        );
+    });
 
-  //방에 완전히 끊겼을 때
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
+    //방에 완전히 끊겼을 때
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
 
-  socket.on("new_msg", (msg, room, done) => {
-    socket.to(room).emit("new_msg", `${socket.nickname}: ${msg}`);
-    done();
-  });
+    socket.on("new_msg", (msg, room, done) => {
+        socket.to(room).emit("new_msg", `${socket.nickname}: ${msg}`);
+        done();
+    });
 
-  socket.on("nickname", (nick) => {
-    socket["nickname"] = nick;
-  });
+    socket.on("nickname", (nick) => {
+        socket["nickname"] = nick;
+    });
 });
 
 httpServer.listen(3000, handleListen);
