22b5523f505492c7ebf12ceca525dd415c06d24d
SGNAN <giananews@gmail.com>
2022-08-21 14:20:27 +0900

    ready for merging

--- icanseeyou/settings.py
+++ icanseeyou/settings.py
@@ -99,12 +99,27 @@ USE_I18N = True
 
 USE_TZ = False
 
-
+TEMPLATES = [
+    {
+        'BACKEND':'django.template.backends.django.DjangoTemplates',
+        'DIRS' : [BASE_DIR, 'templates'],
+        'APP_DIRS' : True,
+        'OPTIONS':{
+            'context_processors':[
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
---
+++ icanseeyou/static/js/app.js
@@ -0,0 +1,277 @@
+const socket = io(); //socketIO를 프론트와 연결
+
+//카메라
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+//채팅창
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+let roomName;
+
+//캠, 오디오를 화면에 출력
+async function getMedia() {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        });
+        myFace.srcObject = myStream;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function takepicture() {
+    let data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
+}
+
+// 카메라 버튼
+function handleCameraClick() {
+    if (cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
+
+// 음소거 버튼
+function handleMuteClick() {
+    if (muted) {
+        muteBtn.innerText = "Unmute";
+        muted = false;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = true;
+    }
+}
+
+// 메시지를 입력받았을 때 페이지에 올려주는 함수
+function addMessage(msg) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = msg;
+    ul.appendChild(li);
+}
+
+// 방에 들어와있을때
+// 메시지를 받았을 때 서버로 옮기기
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_msg", input.value, roomName, () => {
+        addMessage(`You : ${value}`);
+        input.value = "";
+    });
+}
+
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    const value = input.value;
+    socket.emit("nickname", value);
+    input.value = "";
+}
+
+// 해당 방에 들어왔을 때 페이지에서 할 처리
+function showRoom() {
+    const h3 = room.querySelector("h3");
+    welcome.hidden = true;
+    room.hidden = false;
+    h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
+}
+
+//해당 방에 들어왔을 때 처리함수
+function handleRoomSubmit(event) {
+    event.preventDefault();
+    const input = form.querySelector("input");
+    //서버로 원하는 것을 모두 소켓에 담아 보낼 수 있음
+    socket.emit(
+        //백엔드에 펑션 실행스위치를 주는 듯?
+        "room",
+        input.value,
+        showRoom //서버에서 호출되는 펑션
+    );
+    roomName = input.value;
+
+    input.value = "";
+}
+/*--------------------------실행 파트------------------------ */
+getMedia();
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+
+//스트리밍 캡처
+(() => {
+    // The width and height of the captured photo. We will set the
+    // width to the value defined here, but the height will be
+    // calculated based on the aspect ratio of the input stream.
+
+    const width = 320; // We will scale the photo width to this
+    let height = 0; // This will be computed based on the input stream
+
+    // |streaming| indicates whether or not we're currently streaming
+    // video from the camera. Obviously, we start at false.
+
+    let streaming = false;
+
+    // The various HTML elements we need to configure or control. These
+    // will be set by the startup() function.
+
+    let video = null;
+    let canvas = null;
+    let photo = null;
+    let startbutton = null;
+
+    function showViewLiveResultButton() {
+        if (window.self !== window.top) {
+            // Ensure that if our document is in a frame, we get the user
+            // to first open it in its own tab or window. Otherwise, it
+            // won't be able to request permission for camera access.
+            document.querySelector(".contentarea").remove();
+            const button = document.createElement("button");
+            button.textContent = "View live result of the example code above";
+            document.body.append(button);
+            button.addEventListener("click", () => window.open(location.href));
+            return true;
+        }
+        return false;
+    }
+
+    function startup() {
+        if (showViewLiveResultButton()) {
+            return;
+        }
+        video = document.getElementById("video");
+        canvas = document.getElementById("canvas");
+        photo = document.getElementById("photo");
+        startbutton = document.getElementById("startbutton");
+
+        navigator.mediaDevices
+            .getUserMedia({ video: true, audio: false })
+            .then((stream) => {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch((err) => {
+                console.error(`An error occurred: ${err}`);
+            });
+
+        video.addEventListener(
+            "canplay",
+            (ev) => {
+                if (!streaming) {
+                    height = video.videoHeight / (video.videoWidth / width);
+
+                    // Firefox currently has a bug where the height can't be read from
+                    // the video, so we will make assumptions if this happens.
+
+                    if (isNaN(height)) {
+                        height = width / (4 / 3);
+                    }
+
+                    video.setAttribute("width", width);
+                    video.setAttribute("height", height);
+                    canvas.setAttribute("width", width);
+                    canvas.setAttribute("height", height);
+                    streaming = true;
+                }
+            },
+            false
+        );
+
+        startbutton.addEventListener(
+            "click",
+            (ev) => {
+                takepicture();
+                ev.preventDefault();
+            },
+            false
+        );
+
+        clearphoto();
+    }
+
+    // Fill the photo with an indication that none has been
+    // captured.
+
+    function clearphoto() {
+        const context = canvas.getContext("2d");
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        const data = canvas.toDataURL("image/png");
+        photo.setAttribute("src", data);
+    }
+
+    // Capture a photo by fetching the current contents of the video
+    // and drawing it into a canvas, then converting that to a PNG
+    // format data URL. By drawing it on an offscreen canvas and then
+    // drawing that to the screen, we can change its size and/or apply
+    // other changes before drawing it.
+
+    function takepicture() {
+        const context = canvas.getContext("2d");
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+
+            const data = canvas.toDataURL("image/png");
+            photo.setAttribute("src", data);
+        } else {
+            clearphoto();
+        }
+    }
+
+    // Set up our event listener to run the startup process
+    // once loading is complete.
+    window.addEventListener("load", startup, false);
+})();
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`${user} joined!`);
+});
+
+socket.on("bye", (left, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`${left} left ㅜㅜ`);
+});
+
+socket.on("new_msg", addMessage);
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if (rooms.length === 0) {
+        return;
+    }
+    rooms.forEach((room) => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.appendChild(li);
+    });
+});
